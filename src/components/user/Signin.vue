c36d7c880ab3b3c8ef4258bcf43967d06e0a76d5
b16cs006 <meena.13@yahoo.com>
2019-04-21 02:28:12 +0530

    Adding loading in singin

--- src/components/user/Signin.vue
+++ src/components/user/Signin.vue
@@ -1,10 +1,10 @@
 <template>
-<v-container>
-  <!-- <v-dialog max-width="500"> -->
+  <v-container>
+    <!-- <v-dialog max-width="500"> -->
     <!-- <v-btn flat slot="activator">Sign In</v-btn> -->
     <v-layout row v-if="error">
       <v-flex xs12 sm6 offset-sm3>
-        <app-alert @dismissed="onDismissed" :text='error.message'></app-alert>
+        <app-alert @dismissed="onDismissed" :text="error.message"></app-alert>
       </v-flex>
     </v-layout>
     <v-card>
@@ -43,60 +43,68 @@
             </v-flex>
           </v-layout>
           <v-layout row justify-space-between>
-              <v-btn flat color="warning" type="reset">Reset</v-btn>
-              <v-btn flat color="success" type="submit">Sign In</v-btn>
+            <v-btn flat color="warning" type="reset">Reset</v-btn>
+            <v-btn flat color="success" type="submit" :disabled="loading" :loading="loading">
+              Sign In
+              <span class="custom-loader" slot="loader">
+                <v-icon light>cached</v-icon>
+              </span>
+            </v-btn>
           </v-layout>
         </v-form>
       </v-card-text>
     </v-card>
-  <!-- </v-dialog> -->
-</v-container>
+    <!-- </v-dialog> -->
+  </v-container>
 </template>
 
 <script>
-import Mixins from '../validationMixins.js'
+import Mixins from "../validationMixins.js";
 
 export default {
   data() {
     return {
       signinemail: "",
-      signinpassword: "",
+      signinpassword: ""
     };
   },
-  computed:{
-    currentUser(){
-      return this.$store.getters.currentUser
+  computed: {
+    currentUser() {
+      return this.$store.getters.currentUser;
     },
-    error(){
-      return this.$store.getters.error
+    error() {
+      return this.$store.getters.error;
+    },
+    loading(){
+      return this.$store.getters.loading
     }
   },
-  watch:{
-    currentUser(value){
-      if(value !== null && value !== undefined){
-        this.$router.push('/')
+  watch: {
+    currentUser(value) {
+      if (value !== null && value !== undefined) {
+        this.$router.push("/");
       }
     }
   },
   methods: {
     onSignIn() {
       if (this.$refs.form.validate()) {
-        const payload ={
+        const payload = {
           email: this.signinemail,
-          password: this.signinpassword,
-        }
-        this.$store.dispatch('signIn',payload)
+          password: this.signinpassword
+        };
+        this.$store.dispatch("signIn", payload);
       }
     },
-    onDismissed(){
+    onDismissed() {
       // console.log('dismissed')
-      this.$store.dispatch('clearError')
+      this.$store.dispatch("clearError");
     }
   },
-  created(){
-    this.onDismissed()
+  created() {
+    this.onDismissed();
   },
-  mixins:[Mixins]
+  mixins: [Mixins]
 };
 </script>
 
